049ee4df78c9295ce1afe411fe31234732e60ebd
t-sin <shinichi.tanaka45@gmail.com>
2020-12-31 01:49:58 +0900

    Add simple documentations

--- crates/apu/register.rs
+++ crates/apu/register.rs
@@ -1,10 +1,14 @@
+//! Types and functions for GameBoy's APU registers.
+
+/// Sweep-related paramaters for square wave channel.
 #[derive(Debug)]
 pub struct SquareSweep {
     period: u8,
-    nagete: bool,
+    nagate: bool,
     shift: u8,
 }
 
+/// Envelope generator paramaters for some channels.
 #[derive(Debug)]
 pub struct Envelope {
     starting_volume: u8,
@@ -12,6 +16,7 @@ pub struct Envelope {
     envelope_period: u8,
 }
 
+/// Available values of Wavetable channel volume.
 #[derive(Debug)]
 pub enum VolumeCode {
     Percent0,
@@ -20,54 +25,62 @@ pub enum VolumeCode {
     Percent100,
 }
 
+/// Frequency in Hz for oscillators pitch.
 #[derive(Debug)]
-pub struct Pitch {
+pub struct Frequency {
     frequency: u16,
 }
 
+/// Event-related statuses and paramaters.
 #[derive(Debug)]
 pub struct Event {
     trigger: bool,
     length_enable: bool,
 }
 
+/// Parameters for both (square1 and square2) square wave channel.
 #[derive(Debug)]
 pub struct SquareCommon {
     duty: u8,
     length_load: u8,
     envelope: Envelope,
-    pitch: Pitch,
+    frequency: Frequency,
     event: Event,
 }
 
+/// Square1 channel. It can sweep frequency.
 #[derive(Debug)]
 pub struct Square1 {
     sweep: SquareSweep,
     common: SquareCommon,
 }
 
+/// Square2 channel. It is without frequency sweeping.
 #[derive(Debug)]
 pub struct Square2 {
     common: SquareCommon,
 }
 
+/// Wavetable channel.
 #[derive(Debug)]
 pub struct Wave {
     dac_power: bool,
     length_load: u8,
     volume: VolumeCode,
-    pitch: Pitch,
+    frequency: Frequency,
     event: Event,
 }
 
+/// Noise channel.
 #[derive(Debug)]
 pub struct Noise {
     length_load: u8,
     envelope: Envelope,
-    pitch: Pitch,
+    frequency: Frequency,
     event: Event,
 }
 
+/// Statases of all (four) channels.
 #[derive(Debug)]
 pub struct Status {
     square1: bool,
@@ -76,6 +89,7 @@ pub struct Status {
     noise: bool,
 }
 
+/// Registers for controlling and checking statuses.
 #[derive(Debug)]
 pub struct Control {
     vin_l_enabled: bool,
@@ -88,11 +102,13 @@ pub struct Control {
     power_control: Status,
 }
 
+/// Waveform table itself for Wave channel.
 #[derive(Debug)]
 pub struct WaveTable {
     table: [u8; 32],
 }
 
+/// All registers of GameBoy's APU.
 #[derive(Debug)]
 pub struct Register {
     square1: Square1,
