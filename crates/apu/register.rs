9ee30d8b7b5513c29b37e54b9f97f13e75bacb12
t-sin <shinichi.tanaka45@gmail.com>
2020-12-31 11:25:37 +0900

    Add initializing code

--- crates/apu/register.rs
+++ crates/apu/register.rs
@@ -8,7 +8,7 @@ pub enum RegisterError {
 
 /// Frequency-sweeping-related paramaters for square wave channel.
 #[derive(Debug)]
-pub struct SquareSweep {
+pub struct Sweep {
     /// Sweeping speed. 3 bits.
     pub period: u8,
     /// A modifier for frequency calculation. 1 bits.
@@ -17,7 +17,15 @@ pub struct SquareSweep {
     pub shift: u8,
 }
 
-impl SquareSweep {
+impl Sweep {
+    pub fn init() -> Sweep {
+        Sweep {
+            period: 0,
+            negate: false,
+            shift: 0,
+        }
+    }
+
     pub fn set_period(&mut self, period: u32) -> Result<(), RegisterError> {
         if within(period.into(), 3) {
             self.period = period as u8;
@@ -49,6 +57,14 @@ pub struct Envelope {
 }
 
 impl Envelope {
+    pub fn init() -> Envelope {
+        Envelope {
+            starting_volume: 0,
+            add_mode: false,
+            period: 0,
+        }
+    }
+
     pub fn set_starting_volume(&mut self, starting_volume: u32) -> Result<(), RegisterError> {
         if within(starting_volume.into(), 4) {
             self.starting_volume = starting_volume as u8;
@@ -73,7 +89,7 @@ impl Envelope {
 
 /// Available values of Wavetable channel volume.
 #[derive(Debug)]
-pub enum VolumeCode {
+pub enum Volume {
     Percent0,
     Percent25,
     Percent50,
@@ -88,6 +104,10 @@ pub struct Frequency {
 }
 
 impl Frequency {
+    pub fn init() -> Frequency {
+        Frequency { frequency: 0 }
+    }
+
     pub fn set_frequency(&mut self, f: u32) -> Result<(), RegisterError> {
         if within(f, 11) {
             self.frequency = f as u16;
@@ -103,6 +123,15 @@ impl Frequency {
 pub struct Event {
     pub trigger: bool,
     pub length_enable: bool,
+}
+
+impl Event {
+    pub fn init() -> Event {
+        Event {
+            trigger: false,
+            length_enable: false,
+        }
+    }
 }
 
 /// Available duty ratio for square oscillators.
@@ -127,6 +156,16 @@ pub struct SquareCommon {
 }
 
 impl SquareCommon {
+    pub fn init() -> SquareCommon {
+        SquareCommon {
+            duty: Duty::Percent50,
+            length_load: 0,
+            envelope: Envelope::init(),
+            frequency: Frequency::init(),
+            event: Event::init(),
+        }
+    }
+
     pub fn set_length_load(&mut self, length_load: u32) -> Result<(), RegisterError> {
         if within(length_load, 6) {
             self.length_load = length_load as u8;
@@ -140,7 +179,7 @@ impl SquareCommon {
 /// Square1 channel. It can sweep frequency.
 #[derive(Debug)]
 pub struct Square1 {
-    pub sweep: SquareSweep,
+    pub sweep: Sweep,
     pub common: SquareCommon,
 }
 
@@ -155,12 +194,22 @@ pub struct Square2 {
 pub struct Wave {
     dac_power: bool,
     length_load: u8,
-    volume: VolumeCode,
+    volume: Volume,
     frequency: Frequency,
     event: Event,
 }
 
 impl Wave {
+    pub fn init() -> Wave {
+        Wave {
+            dac_power: false,
+            length_load: 0,
+            volume: Volume::Percent50,
+            frequency: Frequency::init(),
+            event: Event::init(),
+        }
+    }
+
     pub fn set_length_load(&mut self, length_load: u32) -> Result<(), RegisterError> {
         if within(length_load, 8) {
             self.length_load = length_load as u8;
@@ -181,6 +230,15 @@ pub struct Noise {
 }
 
 impl Noise {
+    pub fn init() -> Noise {
+        Noise {
+            length_load: 0,
+            envelope: Envelope::init(),
+            frequency: Frequency::init(),
+            event: Event::init(),
+        }
+    }
+
     pub fn set_length_load(&mut self, length_load: u32) -> Result<(), RegisterError> {
         if within(length_load, 8) {
             self.length_load = length_load as u8;
@@ -238,6 +296,14 @@ pub struct WaveTable {
     pub table: [u8; 16],
 }
 
+impl WaveTable {
+    pub fn init() -> WaveTable {
+        WaveTable {
+            table: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        }
+    }
+}
+
 /// All registers of GameBoy's APU.
 #[derive(Debug)]
 pub struct Register {
@@ -246,4 +312,21 @@ pub struct Register {
     pub wave: Wave,
     pub noise: Noise,
     pub wavetable: WaveTable,
+}
+
+impl Register {
+    pub fn init() -> Register {
+        Register {
+            square1: Square1 {
+                sweep: Sweep::init(),
+                common: SquareCommon::init(),
+            },
+            square2: Square2 {
+                common: SquareCommon::init(),
+            },
+            wave: Wave::init(),
+            noise: Noise::init(),
+            wavetable: WaveTable::init(),
+        }
+    }
 }
