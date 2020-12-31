3acd9c5a0ce9385c1d36ce3d0d837111d7f7b9e5
t-sin <shinichi.tanaka45@gmail.com>
2020-12-31 09:45:23 +0900

    Update crate doc of APU

--- crates/apu/lib.rs
+++ crates/apu/lib.rs
@@ -1,3 +1,8 @@
+//! This crate provides GameBoy's APU emulation. It includes APU controlling API via *registers* and sound processing emulation layer. The emulation layer has no dependency about platform-specific sound I/O API e.g. ALSA for GNU/Linux.
+//!
+//! References:
+//! - <https://gbdev.gg8.se/wiki/articles/Gameboy_sound_hardware>
+
 pub mod register;
 
 #[derive(Debug)]
