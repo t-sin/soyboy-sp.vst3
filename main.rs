58728d1c5fd70ef167767d78a96d15c0b1263127
t-sin <shinichi.tanaka45@gmail.com>
2020-12-31 11:31:23 +0900

    Print APU

--- main.rs
+++ main.rs
@@ -1,1 +1,9 @@
-fn main() {}
+use gbi_apu::register::Register;
+use gbi_apu::APU;
+
+fn main() {
+    let apu = APU {
+        registers: Register::init(),
+    };
+    println!("apu = {:?}", apu);
+}
