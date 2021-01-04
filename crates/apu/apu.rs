47a471a89f03ebce691351439d681faaf081a2c9
t-sin <shinichi.tanaka45@gmail.com>
2021-01-05 01:34:31 +0900

    Specify output signal type

--- crates/apu/apu.rs
+++ crates/apu/apu.rs
@@ -1,5 +1,32 @@
 use crate::register::Register;
 
+/// Type of output sample including two channel values.
+pub struct Sample(u8);
+
+impl Sample {
+    pub fn create(left: u8, right: u8) -> Sample {
+        let l = (left << 4) & 0b11110000;
+        let r = right & 0b00001111;
+
+        Sample(l | r)
+    }
+
+    pub fn left(&self) -> u8 {
+        self.0 & 0b11110000
+    }
+
+    pub fn right(&self) -> u8 {
+        self.0 & 0b00001111
+    }
+
+    pub fn add(&self, other: &Self) -> Self {
+        let l = self.left() + other.left();
+        let r = self.right() + other.right();
+
+        Self::create(l, r)
+    }
+}
+
 #[derive(Debug)]
 pub struct APU {
     pub registers: Register,
