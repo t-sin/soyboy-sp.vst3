25d7b63ebc44b7223c3ea2978e93306f835d7505
t-sin <shinichi.tanaka45@gmail.com>
2021-09-14 09:47:25 +0900

    Make square wave osc a module

--- src/gbi/mod.rs
+++ src/gbi/mod.rs
@@ -1,37 +1,8 @@
+mod square_wave;
 mod types;
 
-use types::u4;
+use square_wave::SquareWaveOscillator;
 pub use types::AudioProcessor;
-
-fn pulse(phase: f64, duty: f64) -> u4 {
-    let ph = phase % 1.0;
-    if ph < duty {
-        u4::MIN
-    } else {
-        u4::MAX
-    }
-}
-
-pub struct SquareWaveOscillator {
-    pub phase: f64,
-}
-
-impl SquareWaveOscillator {
-    pub fn new() -> Self {
-        SquareWaveOscillator { phase: 0.0 }
-    }
-}
-
-impl AudioProcessor<u4> for SquareWaveOscillator {
-    fn process(&mut self, sample_rate: f64) -> u4 {
-        let freq = 440.0;
-        let phase_diff = (freq / sample_rate) / 2.0;
-
-        let v = pulse(self.phase, 0.5);
-        self.phase += phase_diff;
-        v
-    }
-}
 
 pub type Signal = (f64, f64);
 
