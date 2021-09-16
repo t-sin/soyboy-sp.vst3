93fe1704f3ee638b77aa68d46bc33b9e760beda2
t-sin <shinichi.tanaka45@gmail.com>
2021-09-16 23:15:11 +0900

    Move constants and Add plugin parameters

--- src/constant.rs
+++ src/constant.rs
@@ -1,12 +1,1 @@
-pub const VST3_VENDOR: &str = "t-sin";
-pub const VST3_URL: &str = "https://github.com/t-sin/gbi/";
-pub const VST3_EMAIL: &str = "shinichi.tanaka45@gmail.com";
 
-pub const VST3_CLASS_NAME: &str = "gbi";
-pub const VST3_CLASS_VERSION: &str = "0.1.0";
-pub const VST3_CLASS_CATEGORY: &str = "Audio Module Class";
-pub const VST3_CLASS_SUBCATEGORIES: &str = "Instrument|Synth";
-
-pub const VST3_CID: [u8; 16] = [
-    0xd6, 0x8e, 0x5c, 0xd2, 0x8a, 0x5d, 0x4d, 0xbe, 0xaf, 0xfa, 0x4a, 0x3f, 0x01, 0xfc, 0x93, 0xd1,
-];
--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,6 @@ extern crate env_logger;
 extern crate vst3_com;
 extern crate vst3_sys;
 
-mod constant;
 mod gbi;
 mod vst3;
 
