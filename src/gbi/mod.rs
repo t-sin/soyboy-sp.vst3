99b62f232faee82a4131b03168596264fa54a704
t-sin <shinichi.tanaka45@gmail.com>
2021-09-20 08:27:35 +0900

    Add parameter master volume

--- src/gbi/mod.rs
+++ src/gbi/mod.rs
@@ -12,6 +12,7 @@ pub type Signal = (f64, f64);
 pub struct GameBoyInstrument {
     square_osc: SquareWaveOscillator,
     envelope_gen: EnvelopeGenerator,
+    master_volume: f64,
 }
 
 impl AudioProcessor<Signal> for GameBoyInstrument {
@@ -19,7 +20,7 @@ impl AudioProcessor<Signal> for GameBoyInstrument {
         let osc = self.square_osc.process(sample_rate).to_f64();
         let env = self.envelope_gen.process(sample_rate);
 
-        let signal = osc * env * 0.5;
+        let signal = osc * env * self.master_volume;
         (signal, signal)
     }
 }
@@ -29,7 +30,12 @@ impl GameBoyInstrument {
         GameBoyInstrument {
             square_osc: SquareWaveOscillator::new(),
             envelope_gen: EnvelopeGenerator::new(),
+            master_volume: 1.0,
         }
+    }
+
+    pub fn set_volume(&mut self, volume: f64) {
+        self.master_volume = volume;
     }
 
     pub fn note_on(&mut self, pitch: i16) {
