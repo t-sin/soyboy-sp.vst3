d76042aa94b8cf1352c3ebe1556354d0ce8ad142
t-sin <shinichi.tanaka45@gmail.com>
2021-11-10 21:53:00 +0900

    Rename plugin directory

--- create_vst3.sh
+++ create_vst3.sh
@@ -2,9 +2,9 @@
 
 cargo build
 
-mkdir -p target/debug/SoyBoySP.vst3
-mkdir -p target/debug/SoyBoySP.vst3/Contents
-mkdir -p target/debug/SoyBoySP.vst3/Contents/Resources
-mkdir -p target/debug/SoyBoySP.vst3/Contents/x86_64-linux
+mkdir -p target/debug/soyboy-sp.vst3
+mkdir -p target/debug/soyboy-sp.vst3/Contents
+mkdir -p target/debug/soyboy-sp.vst3/Contents/Resources
+mkdir -p target/debug/soyboy-sp.vst3/Contents/x86_64-linux
 
-cp target/debug/libsoyboy_sp.so target/debug/SoyBoySP.vst3/Contents/x86_64-linux/SoyBoySP.so
+cp target/debug/libsoyboy_sp.so target/debug/soyboy-sp.vst3/Contents/x86_64-linux/soyboy-sp.so
