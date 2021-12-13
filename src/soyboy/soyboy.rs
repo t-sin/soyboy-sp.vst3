14bf78747aab36deb6e7a0070c2100ce432736ab
t-sin <shinichi.tanaka45@gmail.com>
2021-12-13 23:55:59 +0900

    Use `Self`

--- src/soyboy/soyboy.rs
+++ src/soyboy/soyboy.rs
@@ -57,8 +57,8 @@ pub struct SoyBoy {
 }
 
 impl SoyBoy {
-    pub fn new() -> SoyBoy {
-        SoyBoy {
+    pub fn new() -> Self {
+        Self {
             freq: 0.0,
 
             square_osc: SquareWaveOscillator::new(),
