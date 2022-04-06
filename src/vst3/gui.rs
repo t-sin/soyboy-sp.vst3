fcae570446cd4ee3e7f346f1fb60a8b8586c5431
t-sin <shinichi.tanaka45@gmail.com>
2022-04-07 00:20:57 +0900

    WIP: A journey to create OpenGL context

--- src/vst3/gui.rs
+++ src/vst3/gui.rs
@@ -1,5 +1,6 @@
 use std::cell::RefCell;
 use std::os::raw::c_void;
+use std::ptr::null_mut;
 use std::thread;
 use std::time::Instant;
 
@@ -15,14 +16,46 @@ use egui_backend::{egui, gl, sdl2};
 use egui_backend::{sdl2::event::Event, DpiScaling, ShaderVersion};
 use egui_sdl2_gl as egui_backend;
 use sdl2::{
+    sys::SDL_Window,
     video::{GLContext, SwapInterval, Window},
     EventPump,
 };
 
 use crate::vst3::utils;
 
+// To modify the flags field of raw windows.
+// source: https://github.com/libsdl-org/SDL/blob/a1e992b110b9adf3305a5ebb5514f0e970f7911e/src/video/SDL_sysvideo.h#L74
+#[repr(C)]
+struct Raw_SDL_Window {
+    magic: *mut c_void,
+    id: u32,
+    title: *mut c_void,
+    icon: *mut c_void,
+    x: u32,
+    y: u32,
+    w: u32,
+    h: u32,
+    min_w: u32,
+    min_h: u32,
+    max_w: u32,
+    max_h: u32,
+    flags: u32,
+    last_fullscreen_flags: u32,
+    display_index: u32,
+}
+
+// To create OpenGL context for raw windows.
+// source: https://github.com/libsdl-org/SDL/blob/a1e992b110b9adf3305a5ebb5514f0e970f7911e/src/video/x11/SDL_x11opengl.c#L696
+extern "C" {
+    fn X11_GL_CreateContext(videodevice: *mut c_void, window: *mut c_void) -> *mut c_void;
+}
+
 const SCREEN_WIDTH: u32 = 800;
 const SCREEN_HEIGHT: u32 = 600;
+
+struct ParentWindow(*mut c_void);
+unsafe impl Send for ParentWindow {}
+unsafe impl Sync for ParentWindow {}
 
 struct GUIThread {
     // SoyBoy specific
@@ -42,7 +75,7 @@ struct GUIThread {
 // from this code originally:
 //   https://github.com/ArjunNair/egui_sdl2_gl/blob/main/examples/basic.rs
 impl GUIThread {
-    fn setup() -> Self {
+    fn setup(parent: ParentWindow) -> Self {
         let sdl_context = sdl2::init().unwrap();
         let video_subsystem = sdl_context.video().unwrap();
         let gl_attr = video_subsystem.gl_attr();
@@ -53,12 +86,9 @@ impl GUIThread {
         gl_attr.set_double_buffer(true);
         gl_attr.set_multisample_samples(4);
 
-        let window = video_subsystem
-            .window("SoyBoy SP v0.5.x", SCREEN_WIDTH, SCREEN_HEIGHT)
-            .opengl()
-            .resizable()
-            .build()
-            .unwrap();
+        let mut window = Self::setup_window(video_subsystem.clone(), parent).unwrap();
+        let _ = window.set_title("SoyBoy SP v0.5.x");
+        let _ = window.set_size(SCREEN_WIDTH, SCREEN_HEIGHT);
 
         // Create a window context
         let gl_context = window.gl_create_context().unwrap();
@@ -81,6 +111,47 @@ impl GUIThread {
             event_pump: event_pump,
             painter: painter,
         }
+    }
+
+    // Do like a WindowBuilder.build() with SDL_CreateWindowFrom().
+    // cf. https://github.com/Rust-SDL2/rust-sdl2/blob/6e078feb1f55f5a8a2f1652d9fae50a167635439/src/sdl2/video.rs#L1092
+    fn setup_window(
+        subsystem: sdl2::VideoSubsystem,
+        parent: ParentWindow,
+    ) -> Result<Window, String> {
+        let raw = unsafe { sdl2::sys::SDL_CreateWindowFrom(parent.0) };
+
+        if raw.is_null() {
+            return Err("A null window is created".to_string());
+        }
+
+        let mut window = unsafe { Window::from_ll(subsystem.clone(), raw) };
+
+        // to enable OpenGL with created by SDL_CreateWindowFrom() (cannot set flags in normally),
+        // read this answer:
+        //
+        // > it uses SDL_CreateWindow internally in SDL_test_common and not SDL_CreateWindowFrom.
+        // > --- https://discourse.libsdl.org/t/sdl-createwindowfrom-with-opengl/19737/5
+        //
+        // and see SDL's source code here:
+        // - https://github.com/libsdl-org/SDL/blob/279aeb59be648f13a5f3f63cd466a6f2b354f206/src/video/SDL_video.c#L1513
+        unsafe {
+            let window = &mut window as *mut _ as *mut Raw_SDL_Window;
+            (*window).flags |= sdl2::sys::SDL_WindowFlags::SDL_WINDOW_OPENGL as u32;
+        }
+        // println!(
+        //     "platform, videosystem = {:?}, {:?}",
+        //     sdl2::get_platform(),
+        //     subsystem.current_video_driver()
+        // );
+        unsafe {
+            let window = &mut window as *mut _ as *mut c_void;
+            // ↓ こいつがないのでDAW起動時のプラグイン読み込み時に弾かれるっピ（・ε・）
+            // X11_GL_CreateContext(null_mut(), window);
+            sdl2::sys::SDL_GL_LoadLibrary(null_mut());
+        }
+
+        Ok(window)
     }
 
     fn update(&mut self) {
@@ -149,8 +220,8 @@ impl GUIThread {
         }
     }
 
-    fn run_loop() {
-        let mut thread = GUIThread::setup();
+    fn run_loop(parent: ParentWindow) {
+        let mut thread = GUIThread::setup(parent);
 
         loop {
             thread.update();
@@ -176,9 +247,9 @@ impl SoyBoyGUI {
         SoyBoyGUI::allocate(handle)
     }
 
-    fn start_gui(&self) {
-        let handle = thread::spawn(|| {
-            GUIThread::run_loop();
+    fn start_gui(&self, parent: ParentWindow) {
+        let handle = thread::spawn(move || {
+            GUIThread::run_loop(parent);
         });
         *self.handle.borrow_mut() = Some(handle);
     }
@@ -213,9 +284,11 @@ impl IPlugView for SoyBoyGUI {
         }
     }
 
-    unsafe fn attached(&self, _parent: *mut c_void, _type_: FIDString) -> tresult {
+    unsafe fn attached(&self, parent: *mut c_void, _type_: FIDString) -> tresult {
         println!("IPlugView::attached()");
-        self.start_gui();
+        let parent = ParentWindow(parent);
+        self.start_gui(parent);
+
         kResultOk
     }
 
