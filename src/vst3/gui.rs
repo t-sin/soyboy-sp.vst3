0d92e90122f57b7dd6cbb8d6075f8cbe9d0b36b4
t-sin <shinichi.tanaka45@gmail.com>
2022-04-08 13:44:33 +0900

    Shutdonw GUI gracefully but restarting GUI fails...

--- src/vst3/gui.rs
+++ src/vst3/gui.rs
@@ -1,6 +1,10 @@
 use std::cell::RefCell;
 use std::os::raw::c_void;
 use std::rc::Rc;
+use std::sync::{
+    mpsc::{channel, Receiver, Sender, TryRecvError},
+    Arc, Mutex,
+};
 use std::thread;
 
 use vst3_sys::{
@@ -17,7 +21,10 @@ use egui_glow::{
 use glutin::{
     event::{Event, WindowEvent},
     event_loop::{ControlFlow, EventLoop, EventLoopBuilder},
-    platform::unix::{EventLoopBuilderExtUnix, WindowBuilderExtUnix},
+    platform::{
+        run_return::EventLoopExtRunReturn,
+        unix::{EventLoopBuilderExtUnix, WindowBuilderExtUnix},
+    },
     window::WindowBuilder,
     PossiblyCurrent, WindowedContext,
 };
@@ -31,12 +38,18 @@ struct ParentWindow(*mut c_void);
 unsafe impl Send for ParentWindow {}
 unsafe impl Sync for ParentWindow {}
 
+enum GUIMessage {
+    Terminate,
+}
+
 struct GUIThread {
     // SoyBoy specific
     slider: f64,
     // window stuff
     quit: bool,
     needs_repaint: bool,
+    // threading stuff
+    receiver: Arc<Mutex<Receiver<GUIMessage>>>,
     // egui stuff
     egui_glow: EguiGlow,
     window: WindowedContext<PossiblyCurrent>,
@@ -46,7 +59,10 @@ struct GUIThread {
 // originally from here:
 //   https://github.com/emilk/egui/blob/7cd285ecbc2d319f1feac7b9fd9464d06a5ccf77/egui_glow/examples/pure_glow.rs
 impl GUIThread {
-    fn setup(parent: ParentWindow) -> (Self, EventLoop<()>) {
+    fn setup(
+        parent: ParentWindow,
+        receiver: Arc<Mutex<Receiver<GUIMessage>>>,
+    ) -> (Self, EventLoop<()>) {
         let parent_id: usize = if parent.0.is_null() {
             0
         } else {
@@ -85,6 +101,7 @@ impl GUIThread {
             slider: 0.0,
             quit: false,
             needs_repaint: false,
+            receiver: receiver,
             egui_glow: egui_glow,
             window: window,
             glow_context: glow_context,
@@ -93,24 +110,13 @@ impl GUIThread {
         (thread, event_loop)
     }
 
-    fn draw(&mut self, control_flow: &mut ControlFlow) {
+    fn draw(&mut self) {
         let mut clear_color = [0.1, 0.1, 0.1];
-
-        *control_flow = if self.quit {
-            ControlFlow::Exit
-        } else if self.needs_repaint {
-            self.window.window().request_redraw();
-            ControlFlow::Poll
-        } else {
-            ControlFlow::Wait
-        };
 
         self.needs_repaint = self.egui_glow.run(self.window.window(), |egui_ctx| {
             egui::SidePanel::left("my_side_panel").show(egui_ctx, |ui| {
                 ui.heading("Hello World!");
-                if ui.button("Quit").clicked() {
-                    self.quit = true;
-                }
+                if ui.button("Quit").clicked() {}
                 ui.color_edit_button_rgb(&mut clear_color);
             });
         });
@@ -133,16 +139,50 @@ impl GUIThread {
     }
 
     fn proc_events(&mut self, event: Event<()>, control_flow: &mut ControlFlow) {
+        match self.receiver.lock().unwrap().try_recv() {
+            Ok(message) => match message {
+                GUIMessage::Terminate => {
+                    println!("try_recv() receive Message::Terminate");
+                    self.quit = true;
+                }
+            },
+            Err(err) => match err {
+                TryRecvError::Empty => {
+                    // println!("try_recv() fails because empty");
+                }
+                TryRecvError::Disconnected => {
+                    println!("try_recv() fails because disconnected");
+                    self.quit = true;
+                }
+            },
+        }
+
         match event {
             // Platform-dependent event handlers to workaround a winit bug
             // See: https://github.com/rust-windowing/winit/issues/987
             // See: https://github.com/rust-windowing/winit/issues/1619
-            Event::RedrawEventsCleared if cfg!(windows) => self.draw(control_flow),
-            Event::RedrawRequested(_) if !cfg!(windows) => self.draw(control_flow),
+            Event::RedrawEventsCleared if cfg!(windows) => {
+                self.draw();
+                if self.needs_repaint {
+                    self.window.window().request_redraw();
+                    *control_flow = ControlFlow::Poll;
+                } else {
+                    *control_flow = ControlFlow::Wait;
+                }
+            }
+            Event::RedrawRequested(_) if !cfg!(windows) => {
+                self.draw();
+                if self.needs_repaint {
+                    self.window.window().request_redraw();
+                    *control_flow = ControlFlow::Poll;
+                } else {
+                    *control_flow = ControlFlow::Wait;
+                }
+            }
 
             Event::WindowEvent { event, .. } => {
                 if matches!(event, WindowEvent::CloseRequested | WindowEvent::Destroyed) {
-                    *control_flow = ControlFlow::Exit;
+                    self.quit = true;
                 }
 
                 if let WindowEvent::Resized(physical_size) = &event {
@@ -152,22 +192,28 @@ impl GUIThread {
                 }
 
                 self.egui_glow.on_event(&event);
-
                 self.window.window().request_redraw(); // TODO: ask egui if the events warrants a repaint instead
             }
             Event::LoopDestroyed => {
+                println!("LoopDestroyed is signaled.");
                 self.egui_glow.destroy();
             }
 
             _ => (),
         }
+
+        if self.quit {
+            *control_flow = ControlFlow::Exit;
+        }
     }
 
-    fn run_loop(parent: ParentWindow) {
-        let (mut thread, event_loop) = GUIThread::setup(parent);
+    fn run_loop(parent: ParentWindow, receiver: Arc<Mutex<Receiver<GUIMessage>>>) {
+        let (mut thread, mut event_loop) = GUIThread::setup(parent, receiver);
 
-        event_loop.run(move |event, _, control_flow| {
-            thread.draw(control_flow);
+        event_loop.run_return(move |event, _, control_flow| {
+            if !thread.quit {
+                thread.draw()
+            };
             thread.proc_events(event, control_flow);
         });
     }
@@ -176,18 +222,27 @@ impl GUIThread {
 #[VST3(implements(IPlugView, IPlugFrame))]
 pub struct SoyBoyGUI {
     handle: RefCell<Option<thread::JoinHandle<()>>>,
+    sender: Arc<Mutex<Sender<GUIMessage>>>,
+    receiver: Arc<Mutex<Receiver<GUIMessage>>>,
 }
 
 impl SoyBoyGUI {
     pub fn new() -> Box<Self> {
         let handle = RefCell::new(None);
+        let (send, resv) = channel();
+        let send = Arc::new(Mutex::new(send));
+        let recv = Arc::new(Mutex::new(resv));
 
-        SoyBoyGUI::allocate(handle)
+        SoyBoyGUI::allocate(handle, send, recv)
     }
 
     fn start_gui(&self, parent: ParentWindow) {
+        let (send, resv) = channel();
+        let recv = Arc::new(Mutex::new(resv));
+        (*self.sender.lock().unwrap()) = send;
+
         let handle = thread::spawn(move || {
-            GUIThread::run_loop(parent);
+            GUIThread::run_loop(parent, recv);
         });
         *self.handle.borrow_mut() = Some(handle);
     }
@@ -222,18 +277,26 @@ impl IPlugView for SoyBoyGUI {
         }
     }
 
-    unsafe fn attached(&self, parent: *mut c_void, _type_: FIDString) -> tresult {
+    unsafe fn attached(&self, parent: *mut c_void, type_: FIDString) -> tresult {
         println!("IPlugView::attached()");
-        let parent = ParentWindow(parent);
-        self.start_gui(parent);
+        let type_ = utils::fidstring_to_string(type_);
 
-        kResultOk
+        if type_ == "X11EmbedWindowID" {
+            let parent = ParentWindow(parent);
+            self.start_gui(parent);
+            kResultOk
+        } else {
+            kResultFalse
+        }
     }
 
     unsafe fn removed(&self) -> tresult {
         println!("IPlugView::removed()");
         let old_handle = self.handle.replace(None);
-        let _ = old_handle.unwrap().join();
+        let _ = self.sender.lock().unwrap().send(GUIMessage::Terminate);
+        println!("sended terminate.");
+        let res = old_handle.unwrap().join();
+        println!("joined: {:?}", res);
         kResultOk
     }
     unsafe fn on_wheel(&self, _distance: f32) -> tresult {
