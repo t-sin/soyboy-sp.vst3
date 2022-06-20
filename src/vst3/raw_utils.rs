26017138a87f4004e32e2aa597e3ab6ea2672d27
t-sin <shinichi.tanaka45@gmail.com>
2022-06-20 14:29:54 +0900

    Remove unneeded conversion

--- src/vst3/raw_utils.rs
+++ src/vst3/raw_utils.rs
@@ -19,8 +19,6 @@ pub unsafe fn strcpy(src: &str, dst: *mut c_char) {
 
 pub unsafe fn wstrcpy(src: &str, dst: *mut c_short) {
     let src = U16CString::from_str(src).unwrap();
-    let mut src = src.into_vec();
-    src.push(0);
     copy_nonoverlapping(src.as_ptr() as *const c_void as *const _, dst, src.len());
 }
 
